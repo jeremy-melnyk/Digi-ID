5be0c06bfdddaa3d62b6ced4fe583d9bf575fcfb
Philippe Laferriere <laferriere.phil@gmail.com>
2016-01-13 15:33:20 -0800

    Milestone in generating html. Now need to focus on making big words not overlap

--- MenuReader/MenuReader/HtmlGenerator.cs
+++ MenuReader/MenuReader/HtmlGenerator.cs
@@ -41,7 +41,33 @@ namespace MenuReader
 
             ImageReader imageReader = new ImageReader(this.Card);
             OcrResults results = await imageReader.GetImageAnalysisResultsAsync();
-
+            // TODO: Get image dimensions from outside
+            string wrapperDiv = "    <div style=\"position: relative; " +
+                                                "width: " + 1024 + "px; " +
+                                                "height: " + 665 + "px; " +
+                                                "border: 1px solid black; border-radius: 10px\">\n";
+            await FileIO.AppendTextAsync(this.htmlFile, wrapperDiv);
+            BoundaryBox bBox;
+            foreach (Region reg in results.Regions)
+            {
+                foreach (Line line in reg.Lines)
+                {
+                    foreach (Word word in line.Words)
+                    {
+                        bBox = new BoundaryBox(word.BoundingBox);
+                        string htmlWord = "<span style=\"position: absolute; " +
+                                                        "left: " + bBox.x + "px; " +
+                                                        "top: " + bBox.y + "px; " +
+                                                        "width: " + bBox.Width + "px; " +
+                                                        "height: " + bBox.Height + "px; " +
+                                                        "font-size: " + bBox.Height + "px;\">" + /* super-awesome hack */
+                                                        word.Text + "</span>\n";
+                        await FileIO.AppendTextAsync(this.htmlFile, htmlWord);
+                    }
+                }
+            }
+            // TODO: Add picture
+            await FileIO.AppendTextAsync(this.htmlFile, "</div>");
             t = endFileAsync();
             await t;
         }
@@ -55,6 +81,7 @@ namespace MenuReader
                           "  <body>\n" +
                           "    <h1>Never forget: with great power comes great responsibility</h1>\n";
             await FileIO.WriteTextAsync(this.htmlFile, init);
+
         }
 
         private async Task endFileAsync()
@@ -75,6 +102,23 @@ namespace MenuReader
         private void writeIdPicture()
         {
             // TODO: add picture to html with img tag
+        }
+
+        private class BoundaryBox
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public BoundaryBox(string box)
+            {
+                string[] splitStr = box.Split(new Char[] { ',' });
+                this.x = Int32.Parse(splitStr[0]);
+                this.y = Int32.Parse(splitStr[1]);
+                this.Width = Int32.Parse(splitStr[2]);
+                this.Height = Int32.Parse(splitStr[3]);
+            }
         }
     }
 }
